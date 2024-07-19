df51d7f15095647336943050f02646bc2ab9cd20
SemiCompetentCoder <159509263+SemiCompetentCoder@users.noreply.github.com>
2024-07-19 17:05:40 -0600

    Update clsItemsLogic.cs

--- Items/clsItemsLogic.cs
+++ Items/clsItemsLogic.cs
@@ -34,5 +34,29 @@ namespace DummyWPF.Items
         /// Line Items model
         /// </summary>
         private modLineItems _LineItems;
+
+        // A list containing the IDs of modified line items will indicate if a line item has been modified
+        // might be implemented like so
+        // List<int> modifiedLineItems = new List<int>();
+        
+        public void getLineItems() {
+            ; // get line items from the database and return them as a list
+        }
+        
+        public void submitLineItem() 
+        {
+            ; // will check a line item with the specified code already exists, if it does, it will update it with new information
+              // otherwise, it will simply insert a new one
+        }
+        
+        public void removeLineItem() 
+        {
+            ; // remove the line item, if it exists, and add it to the modifiedLineItems list
+        }
+        
+        public void getModifiedLineItems() 
+        {
+            ; // will get IDs from modifiedLineItems, and then clear the list
+        }
     }
 }
